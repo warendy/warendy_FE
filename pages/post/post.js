e95068c6ea6403da9ab1e1913a1448e968bfec21
soozii <tndwjd963@gmail.com>
2023-07-25 03:43:45 +0900

    Feat 주변 와인샵 검색 페이지 구현

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styles from "../post/Post.module.css";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const seoulOptions = ["강남구", "동작구", "서초구", "관악구", "강북구"];
 const gyeonggiOptions = ["용인시", "수원시", "성남시", "안양시", "고양시"];
@@ -48,46 +50,51 @@ export default function PostSearch() {
     <>
       <h2 className="top">동행 게시글</h2>
       <div className="inner">
-        <select
-          className={styles.select}
-          value={selectedLocation}
-          onChange={handleLocationChange}
-        >
-          <option value="">전체</option>
-          <option value="서울">서울</option>
-          <option value="경기">경기</option>
-        </select>
-
-        {selectedLocation && (
-          <select className={styles.select} value={selectedOption}>
-            {selectedOption.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
+        <div className={styles.postWrap}>
+          <select
+            className={styles.select}
+            value={selectedLocation}
+            onChange={handleLocationChange}
+          >
+            <option value="">지역별</option>
+            <option value="서울">서울</option>
+            <option value="경기">경기</option>
           </select>
-        )}
 
-        <input
-          type="text"
-          placeholder="검색어 입력"
-          value={searchInput}
-          onChange={handleInputChange}
-        />
+          {selectedLocation && (
+            <select className={styles.select} value={selectedOption}>
+              {selectedOption.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          )}
 
-        <button className={styles.button} onClick={handleSearch}>
-          검색
-        </button>
+          <input
+            type="text"
+            placeholder="검색어 입력"
+            value={searchInput}
+            onChange={handleInputChange}
+            className={styles.Input}
+          />
+
+          <button className={styles.button} onClick={handleSearch}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+
+          <button className={styles.myBtn}>내가 쓴 글</button>
+        </div>
+
+        {/* 검색 결과 표시 */}
+        <ul className={styles.list}>
+          {searchResults.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-
-      {/* 검색 결과 표시 */}
-      <ul className={styles.list}>
-        {searchResults.map((post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
     </>
   );
 }
