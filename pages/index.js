2dfba321d8f845c30f29bbbb519effe3d926c1f8
Daily-manjoo <yuslz35@gmail.com>
2023-07-25 17:55:43 +0900

    Feat 인덱스 페이지 수정

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,14 @@ import Header from "../components/header/randingheader";
 import RandingPage from "./randing-page/randingPage";
 import Darkscreen from "./darkscreen/darkscreen";
 import styles from "./darkscreen/darkscreen.module.css"; // Correct import path
-
+import Main from "./main/main";
 import "../styles/main.css";
 import "../styles/reset.css";
 
 export default function IndexPage() {
   return (
     <>
-      <div className="inner">
-        <Header />
-        <RandingPage />
-      </div>
-      <div className={styles.fullscreen}>
-        <Darkscreen />
-      </div>
+      <Main />
     </>
   );
 }
