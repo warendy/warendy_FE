5a1ea8176ed861243e02a0079458b2a094f0838d
soozii <tndwjd963@gmail.com>
2023-07-28 05:40:11 +0900

    Feat 와인숍 찾기 페이지 구현

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import React from "react";
-import MyPage from "./my/my-page.js";
-import WineCollection from "./my/wine-collection.js";
 import Main from "./main/main";
-import "../styles/main.css";
-import "../styles/reset.css";
 
 export default function IndexPage() {
   return (
@@ -12,5 +8,3 @@ export default function IndexPage() {
     </>
   );
 }
-
-
