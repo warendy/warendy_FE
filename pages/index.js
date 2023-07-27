7fea9110ef42e3a892046c7218e3ed893d37d8ba
Kyungjin Jung <123256640+Februaar@users.noreply.github.com>
2023-07-27 16:09:46 +0900

    Update 코드 수정
    Update 코드 수정

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import MyPage from "./my/my-page.js";
-import WineCollection from "./my/wine-collection.js";
 import Main from "./main/main";
-import "../styles/main.css";
-import "../styles/reset.css";
 
-export default function IndexPage() {
+export default function Index() {
   return (
     <>
       <Main />
