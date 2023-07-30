edcedf3321431e339f2029a39e67ea15edb1350d
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-31 00:16:03 +0900

    Add 레이아웃 컴포넌트

--- pages/my/my-page.js
+++ pages/my/my-page.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import styles from "./my-page.module.css";
 
+import Layout from "../../components/layout/layout";
 import My from "./my";
 import Profile from "./profile";
 import ProfileEdit from "./profile-edit";
@@ -8,9 +9,9 @@ import ProfileEdit from "./profile-edit";
 const MyPage = () => {
   const [selectedPage, setSelectedPage] = useState("my");
 
-  const handleSnbLinkClick = (page) => {
+  const handleSnbLinkClick = useCallback((page) => {
     setSelectedPage(page);
-  };
+  }, []);
 
   const renderContentArea = () => {
     switch (selectedPage) {
@@ -27,44 +28,40 @@ const MyPage = () => {
   };
 
   return (
-    <>
-      <div className="container">
-        <div className="inner padding">
-          <div className={styles.snbArea}>
-            <button
-              onClick={() => handleSnbLinkClick("my")}
-              className="resetBtn btn"
-            >
-              <h2 className={styles.mainTitle}>마이페이지</h2>
-            </button>
-            <nav>
-              <div>
-                <strong className={styles.snbTitle}>내 정보</strong>
-                <ul>
-                  <li className={styles.menuItem}>
-                    <button
-                      onClick={() => handleSnbLinkClick("profile")}
-                      className="resetBtn btn"
-                    >
-                      로그인 정보
-                    </button>
-                  </li>
-                  <li className={styles.menuItem}>
-                    <button
-                      onClick={() => handleSnbLinkClick("profile-edit")}
-                      className="resetBtn btn"
-                    >
-                      프로필 관리
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </nav>
+    <Layout>
+      <div className={styles.snbArea}>
+        <button
+          onClick={() => handleSnbLinkClick("my")}
+          className="resetBtn btn"
+        >
+          <h2 className={styles.mainTitle}>마이페이지</h2>
+        </button>
+        <nav>
+          <div>
+            <strong className={styles.snbTitle}>내 정보</strong>
+            <ul>
+              <li className={styles.menuItem}>
+                <button
+                  onClick={() => handleSnbLinkClick("profile")}
+                  className="resetBtn btn"
+                >
+                  로그인 정보
+                </button>
+              </li>
+              <li className={styles.menuItem}>
+                <button
+                  onClick={() => handleSnbLinkClick("profile-edit")}
+                  className="resetBtn btn"
+                >
+                  프로필 관리
+                </button>
+              </li>
+            </ul>
           </div>
-          <div className={styles.contentArea}>{renderContentArea()}</div>
-        </div>
+        </nav>
       </div>
-    </>
+      <div className={styles.contentArea}>{renderContentArea()}</div>
+    </Layout>
   );
 };
 
--- pages/signup/sign-up.js
+++ pages/signup/sign-up.js
@@ -1,55 +1,51 @@
 import styles from "./sign-up.module.css";
 
+import Layout from "../../components/layout/layout";
+
 const SignIn = () => {
   return (
-    <>
-      <div className="container">
-        <div className="inner padding">
-          <h3 className={styles.mainTitle + " title "}>회원가입</h3>
-          <div className={styles.contentArea + " inner "}>
-            <div className={styles.inputInfo}>
-              <div className={styles.email}>
-                <h3 className={styles.title}>이메일 주소</h3>
-                <div>
-                  <div className={styles.inputArea}>
-                    <input
-                      type="email"
-                      placeholder="예) waren@warendy.com"
-                      className={styles.input + " input "}
-                      autoComplete="off"
-                    />
-                  </div>
-                  <p className={styles.error}>
-                    이메일 주소를 정확히 입력해주세요.
-                  </p>
-                </div>
+    <Layout>
+      <h3 className={styles.mainTitle + " title "}>회원가입</h3>
+      <div className={styles.contentArea + " inner "}>
+        <div className={styles.inputInfo}>
+          <div className={styles.email}>
+            <h3 className={styles.title}>이메일 주소</h3>
+            <div>
+              <div className={styles.inputArea}>
+                <input
+                  type="email"
+                  placeholder="예) waren@warendy.com"
+                  className={styles.input + " input "}
+                  autoComplete="off"
+                />
               </div>
-              <div className={styles.password}>
-                <h3 className={styles.title}>비밀번호</h3>
-                <div>
-                  <div className={styles.inputArea}>
-                    <input
-                      type="password"
-                      placeholder="영문, 숫자, 특수문자 조합 8-16자"
-                      className={styles.input + " input "}
-                      autoComplete="off"
-                    />
-                  </div>
-                </div>
-                <p className={styles.error}>
-                  영문, 숫자, 특수문자를 조합해서 입력해주세요.
-                </p>
-              </div>
-              <div className={styles.btnArea}>
-                <button className={styles.btnSignup + " btn "}>
-                  <span className={styles.text}>가입하기</span>
-                </button>
+              <p className={styles.error}>이메일 주소를 정확히 입력해주세요.</p>
+            </div>
+          </div>
+          <div className={styles.password}>
+            <h3 className={styles.title}>비밀번호</h3>
+            <div>
+              <div className={styles.inputArea}>
+                <input
+                  type="password"
+                  placeholder="영문, 숫자, 특수문자 조합 8-16자"
+                  className={styles.input + " input "}
+                  autoComplete="off"
+                />
               </div>
             </div>
+            <p className={styles.error}>
+              영문, 숫자, 특수문자를 조합해서 입력해주세요.
+            </p>
+          </div>
+          <div className={styles.btnArea}>
+            <button className={styles.btnSignup + " btn "}>
+              <span className={styles.text}>가입하기</span>
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </Layout>
   );
 };
 
