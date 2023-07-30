1728b09f730e8bcc092358feec08e6d547b6c212
Daily-manjoo <yuslz35@gmail.com>
2023-07-31 00:00:54 +0900

    Feat 상세페이지 구현 및 별점기능 구현

--- pages/review/review-page.js
+++ pages/review/review-page.js
@@ -6,7 +6,7 @@ const ReviewPage = () => {
   return (
     <>
       <div className="container">
-        <div className="inner top">
+        <div className="inner padding">
           <h3 className="title">내가 쓴 리뷰</h3>
           <div className={styles.reviewPage}>
             <div className={styles.filter}>
@@ -30,25 +30,14 @@ const ReviewPage = () => {
             <div className={styles.contentArea}>
               <div className={styles.reviewCard}>
                 <div className={styles.img}>
-                  <Image
-                    src="/images/wine.png"
-                    alt="Logo"
-                    width={35}
-                    height={140}
-                  />
+                  <Image src="/images/wine.png" alt="Logo" width={35} height={140} />
                 </div>
                 <div className={styles.review}>
                   <div className={styles.reviewArea}>
                     <div className={styles.date}>리뷰 작성일 : 23.02.07</div>
-                    <div className={styles.wineName}>
-                      프리미티보 디 만두리아
-                    </div>
+                    <div className={styles.wineName}>프리미티보 디 만두리아</div>
                   </div>
-                  <Link
-                    href="/my/my-review"
-                    className={styles.btnReview + " btn outline "}
-                    type="button"
-                  >
+                  <Link href="/my/my-review" className={styles.btnReview + " btn outline "} type="button">
                     리뷰보기
                   </Link>
                 </div>
--- pages/signin/sign-in.js
+++ pages/signin/sign-in.js
@@ -8,52 +8,30 @@ const SignIn = () => {
   return (
     <>
       <div className="container">
-        <div className="inner top">
-          <Image
-            src="/images/logo.svg"
-            alt="Logo"
-            className={styles.logo}
-            width={250}
-            height={50}
-          />
+        <div className="inner padding">
+          <Image src="/images/logo.svg" alt="Logo" className={styles.logo} width={250} height={50} />
           <div className={styles.contentArea + " inner "}>
             <div className={styles.inputInfo}>
               <div className={styles.email}>
                 <h3 className={styles.title}>이메일 주소</h3>
                 <div>
                   <div className={styles.inputArea}>
-                    <input
-                      type="email"
-                      placeholder="예) ozik@ozik.co.kr"
-                      className={styles.input + " input "}
-                      autoComplete="off"
-                    />
+                    <input type="email" placeholder="예) ozik@ozik.co.kr" className={styles.input + " input "} autoComplete="off" />
                     <button type="button" className="input">
-                      <FontAwesomeIcon
-                        icon={faCircleXmark}
-                        className={styles.icon}
-                      />
+                      <FontAwesomeIcon icon={faCircleXmark} className={styles.icon} />
                     </button>
                   </div>
-                  <p className={styles.error}>
-                    이메일 주소를 정확히 입력해주세요.
-                  </p>
+                  <p className={styles.error}>이메일 주소를 정확히 입력해주세요.</p>
                 </div>
               </div>
               <div className={styles.password}>
                 <h3 className={styles.title}>비밀번호</h3>
                 <div>
                   <div className={styles.inputArea}>
-                    <input
-                      type="password"
-                      className="input"
-                      autoComplete="off"
-                    />
+                    <input type="password" className="input" autoComplete="off" />
                   </div>
                 </div>
-                <p className={styles.error}>
-                  영문, 숫자, 특수문자를 조합해서 입력해주세요. (8-16자){" "}
-                </p>
+                <p className={styles.error}>영문, 숫자, 특수문자를 조합해서 입력해주세요. (8-16자) </p>
               </div>
               <div className={styles.btnArea}>
                 <button className={styles.btnSignin + " btn "}>
@@ -63,13 +41,7 @@ const SignIn = () => {
                   이메일 가입
                 </Link>
                 <button className={styles.btnSocial + " btn "}>
-                  <Image
-                    src="/images/kakao.svg"
-                    alt="Logo"
-                    className={styles.social}
-                    width={30}
-                    height={30}
-                  />
+                  <Image src="/images/kakao.svg" alt="Logo" className={styles.social} width={30} height={30} />
                   <span className={styles.kakao}>카카오톡으로 로그인</span>
                 </button>
               </div>
