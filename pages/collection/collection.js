8faae690e60d3c95d887d7842b6977634209d3b3
soozii <tndwjd963@gmail.com>
2023-07-31 02:25:57 +0900

    :memo: Feat 합병시 누락된 문서 추가

--- pages/collection/collection.js
+++ pages/collection/collection.js
@@ -1,66 +1,17 @@
-import React, { useState } from "react";
-import styles from "./collection.module.css";
-import DraggableElement from "./draggable-element.js";
-import DropTarget from "./drop-target.js";
+import React from "react";
+import Image from "next/image";
 
-const Collection = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedFilter, setSelectedFilter] = useState("와인 종류별");
-
-  const handleFilterClick = () => {
-    setShowDropdown(!showDropdown);
-  };
-
-  const handleOptionClick = (option) => {
-    setSelectedFilter(option);
-    setShowDropdown(false);
-  };
-
+const Collection = ({ collection, onRemove }) => {
   return (
-    <div className={styles.myCollection}>
-      <div className={styles.filter}>
-        <div className={styles.filterMain}>
-          <div onClick={handleFilterClick}>
-            <span className={styles.result}>{selectedFilter}</span>
-            <span className={styles.arrow}>{showDropdown ? "▲" : "▼"}</span>
-          </div>
-          {showDropdown && (
-            <div className={styles.dropdownWrapper}>
-              <div className={styles.dropdown}>
-                <div
-                  className={styles.option}
-                  onClick={() => handleOptionClick("와인 종류별")}
-                >
-                  와인 종류별
-                </div>
-                <div
-                  className={styles.option}
-                  onClick={() => handleOptionClick("화이트 와인")}
-                >
-                  화이트 와인
-                </div>
-                <div
-                  className={styles.option}
-                  onClick={() => handleOptionClick("레드 와인")}
-                >
-                  레드 와인
-                </div>
-                <div
-                  className={styles.option}
-                  onClick={() => handleOptionClick("스파클링 와인")}
-                >
-                  스파클링 와인
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-      {/* <Content /> */}
-      <DropTarget />
-      <DraggableElement />
-    </div>
+    <ul>
+      {collection.map((wineItem) => (
+        <li key={wineItem.id}>
+          <Image src={wineItem.imageSrc} alt={wineItem.name} />
+          <button onClick={() => onRemove(wineItem.id)}>제거</button>
+        </li>
+      ))}
+    </ul>
   );
 };
-
+// my
 export default Collection;
