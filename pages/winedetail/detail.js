3f7303b0234a491053bf8763c7ef7e787515f0fd
Daily-manjoo <yuslz35@gmail.com>
2023-08-06 23:24:45 +0900

    ✨ 상세페이지 구현

--- pages/winedetail/detail.js
+++ pages/winedetail/detail.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styles from "./detail.module.css";
-import Image from "next/image";
 import StarRating from "./star-rating";
 import { wineState } from "@/pages/recoil_state";
 import { useRecoilValue } from "recoil";
@@ -8,20 +7,47 @@ import { useRecoilValue } from "recoil";
 export default function Detail() {
   const wine = useRecoilValue(wineState);
   const [ratings, setRatings] = useState([0, 0, 0]);
-  const [reviewText, setReviewText] = useState("");
-
-  // 와인 데이터를 저장할 상태를 추가
+  const [reviewText, setReviewText] = useState(" ");
   const [wineData, setWineData] = useState(null);
+  const wineId = wine.id;
+  const [body, setBody] = useState(0);
+  const [dry, setDry] = useState(0);
+  const [tannin, setTannin] = useState(0);
+  const [acidity, setAcidity] = useState(0);
+
+  const progressStyles = {
+    body: { width: `${(body / 5) * 100}%` },
+    dry: { width: `${(dry / 5) * 100}%` },
+    tannin: { width: `${(tannin / 5) * 100}%` },
+    acidity: { width: `${(acidity / 5) * 100}%` },
+  };
+
+  function getGeneralRegion(region) {
+    const parts = region.split("/");
+    return parts.slice(0, 2).join(" / ");
+  }
 
   useEffect(() => {
-    // wine 상태의 ID 부분을 가져옴
-    const wineId = wine.id;
-
     fetch(`https://warendy.shop/wines/${wineId}/detail`)
-      .then((response) => response.json())
-      .then((data) => setWineData(data))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const region = data.region;
+        const generalRegion = getGeneralRegion(region);
+        setBody(data.body);
+        setDry(data.dry);
+        setTannin(data.tannin);
+        setAcidity(data.acidity);
+        setWineData({ ...data, generalRegion });
+      })
       .catch((error) => console.error(error));
-  }, [wine.id]);
+  }, []);
+
+  const pairingData = wineData?.pairing ? wineData.pairing.split(", ") : [];
 
   const handleSetRating = (reviewIndex, newRating) => {
     setRatings((prevRatings) => {
@@ -34,9 +60,10 @@ export default function Detail() {
   const handleReviewSubmit = (reviewIndex) => {
     const rating = ratings[reviewIndex];
     const text = reviewText;
-
-    setReviewText("");
+    // You may want to do something with the 'rating' and 'text' variables here
+    setReviewText(" ");
   };
+
   return (
     <>
       <div className="container">
@@ -46,66 +73,79 @@ export default function Detail() {
           <div className={styles.detailContainer}>
             <div className={styles.wineImage}>
               <div className={styles.img}>
-                <Image src="/images/winedetail.svg" alt="Logo" layout="responsive" width={100} height={100} />
+                <img src={wineData?.picture || "/images/winedetail.svg"} alt="Wine" style={{ width: "50%", height: "100%" }} />
               </div>
             </div>
             <div className={styles.introBox}>
               <div className={styles.introFirst}>
-                <div className={styles.typeBadge}>White</div>
-                <div className={styles.wineTitle}>프랑스</div>
-                <div className="heartBadge"></div>
+                <div className={styles.fromBadge}>From</div>
+                <div className={styles.wineRegion}>{wineData?.generalRegion}</div>
+                <div className="heartBadge margin"></div>
               </div>
               <div className={styles.introTitle}>{wineData?.wineName}</div>
               <div className={styles.figureBox}>
-                <div className={styles.label}>Bitter</div>
-                <div className={`${styles.progress} ${styles.sweetnessProgress}`}></div>
+                <div className={styles.label}>Light</div>
+                <div className={`${styles.progress} ${styles.bodyProgress}`}>
+                  <div className={styles.progressFill} style={progressStyles.body}></div>
+                </div>
+                <div className={styles.label}>Bold</div>
+              </div>
+              <div className={styles.figureBox}>
+                <div className={styles.label}>Smooth</div>
+                <div className={`${styles.progress} ${styles.dryrogress}`}>
+                  <div className={styles.progressFill} style={progressStyles.dry}></div>
+                </div>
+                <div className={styles.label}>Tannic</div>
+              </div>
+              <div className={styles.figureBox}>
+                <div className={styles.label}>Dry</div>
+                <div className={`${styles.progress} ${styles.tanninProgress}`}>
+                  <div className={styles.progressFill} style={progressStyles.tannin}></div>
+                </div>
                 <div className={styles.label}>Sweet</div>
               </div>
               <div className={styles.figureBox}>
-                <div className={styles.label}>Light</div>
-                <div className={`${styles.progress} ${styles.bodyProgress}`}></div>
-                <div className={styles.label}>Full Body</div>
-              </div>
-              <div className={styles.figureBox}>
-                <div className={styles.label}>Low</div>
-                <div className={`${styles.progress} ${styles.tanninsProgress}`}></div>
-                <div className={styles.label}>High Tannins</div>
-              </div>
-              <div className={styles.figureBox}>
-                <div className={styles.label}>Low</div>
-                <div className={`${styles.progress} ${styles.acidityProgress}`}></div>
-                <div className={styles.label}>High Acidity</div>
+                <div className={styles.label}>Soft</div>
+                <div className={`${styles.progress} ${styles.acidityProgress}`}>
+                  <div className={styles.progressFill} style={progressStyles.acidity}></div>
+                </div>
+                <div className={styles.label}>Acidic</div>
               </div>
 
               <div className={styles.withFood}>
                 <div className={styles.withFoodTitle}>이런 음식과 함께 해요!</div>
                 <div className={styles.foodPairings}>
-                  <div className={styles.circle}>Beef</div>
-                  <div className={styles.circle}>Lamb</div>
-                  <div className={styles.circle}>Poultry</div>
+                  {pairingData.map((item, index) => (
+                    <div key={index} className={styles.circle}>
+                      {item}
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
 
           <div className={styles.detailList}>
-            <div className={styles.detailRegion}>REGION :</div>
-            <div className={styles.detailWinery}>WINERY :</div>
-            <div className={styles.detailRating}>RATING :</div>
-            <div className={styles.detailPrice}>PRICE :</div>
+            <div className={styles.detailVintage}>Vintage : {wineData?.vintage}</div>
+            <div className={styles.detailGrapes}>GRAPES : {wineData?.grapes}</div>
+            <div className={styles.detailWinery}>WINERY : {wineData?.winery}</div>
+            <div className={styles.detailPrice}>PRICE : {wineData?.price}</div>
+            <div className={styles.detailRating}>
+              RATING : <StarRating rating={wineData?.rating} isInteractive={false} />
+            </div>
           </div>
 
           <div className={styles.reviewContainer}>
             <div className={styles.reviewBox}>
-              <StarRating rating={ratings[0]} setRating={(newRating) => handleSetRating(0, newRating)} />
+              <StarRating rating={ratings[0]} isInteractive={false} setRating={(newRating) => handleSetRating(0, newRating)} />
               <div className={styles.reviewComment}>가성비가 좋은 와인은 아닌 것 같았어요,, 그냥 딱 가격 정도의 맛</div>
             </div>
             <div className={styles.reviewBox}>
-              <StarRating rating={ratings[1]} setRating={(newRating) => handleSetRating(1, newRating)} />
+              <StarRating rating={ratings[1]} isInteractive={false} setRating={(newRating) => handleSetRating(1, newRating)} />
               <div className={styles.reviewComment}>가족끼리 같이 먹었는데 어른들은 좀 좋아하셨고 20대에겐 좀 무거운 맛</div>
             </div>
             <div className={styles.reviewBox}>
-              <StarRating rating={ratings[2]} setRating={(newRating) => handleSetRating(2, newRating)} />
+              <StarRating rating={ratings[2]} isInteractive={false} setRating={(newRating) => handleSetRating(2, newRating)} />
               <div className={styles.reviewComment}>
                 스위트 와인이라고 되어 있었지만.. 글쎄요. 너무 씁쓸했어요 ㅜㅜ 단 거 엄청 좋아하시는 분은 비추,,,
               </div>
--- pages/winedetail/star-rating.js
+++ pages/winedetail/star-rating.js
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
 import styles from "./star-rating.module.css";
 
-const StarRating = ({ rating, setRating, reviewText, setReviewText }) => {
+const StarRating = ({ rating, setRating, isInteractive = true }) => {
   const MAX_STARS = 5;
   const [hoveredRating, setHoveredRating] = useState(0);
 
   const handleStarClick = (clickedStarIndex) => {
-    const newRating = clickedStarIndex + 1; //
-    setRating(newRating);
+    if (isInteractive) {
+      const newRating = clickedStarIndex + 1;
+      setRating(newRating);
+    }
   };
 
   const handleStarHover = (hoveredStarIndex) => {
-    setHoveredRating(hoveredStarIndex + 1);
+    if (isInteractive) {
+      setHoveredRating(hoveredStarIndex + 1);
+    }
   };
 
   const handleStarHoverLeave = () => {
-    setHoveredRating(0);
+    if (isInteractive) {
+      setHoveredRating(0);
+    }
   };
 
   return (
     <div className={styles.starRating} onMouseLeave={handleStarHoverLeave}>
       {[...Array(MAX_STARS)].map((_, index) => {
-        const starClass = index < rating ? styles.filled : hoveredRating > index ? styles.filled : styles.unfilled;
+        const starClass = index < rating ? styles.filled : isInteractive && hoveredRating > index ? styles.filled : styles.unfilled;
         return (
           <span
             key={index}
