ac6e3b522c264029c1b6e1282e9af383fac537df
Daily-manjoo <yuslz35@gmail.com>
2023-08-11 01:21:12 +0900

    ✨ Feat 검색바 구현 기능

--- components/header/header.js
+++ components/header/header.js
@@ -5,8 +5,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faComment } from "@fortawesome/free-regular-svg-icons";
 import styles from "./header.module.css";
+import { searchBarState, slideState } from "@/pages/recoil_state";
+import { useSetRecoilState } from "recoil";
 
 const Header = () => {
+  const setSearchBarState = useSetRecoilState(searchBarState);
+  const setAnimationClass = useSetRecoilState(slideState);
+
+  const changeState = () => {
+    setSearchBarState(true);
+    setAnimationClass("slidein");
+  };
   return (
     <header>
       <div className={styles.header}>
@@ -29,39 +38,21 @@ const Header = () => {
             <div className={styles.mainInner}>
               <h1 className={styles.mainLogo}>
                 <Link href="/" className={styles.logo}>
-                  <Image
-                    src="/images/logo.svg"
-                    alt="Logo"
-                    className={styles.img}
-                    width={100}
-                    height={70}
-                  />
+                  <Image src="/images/logo.svg" alt="Logo" className={styles.img} width={100} height={70} />
                 </Link>
               </h1>
               <div className={styles.gnbArea}>
                 <nav className={styles.gnb}>
-                  <Link
-                    href="/search/search"
-                    className={`${styles.gnbItem} ${styles.link}`}
-                  >
-                    <FontAwesomeIcon
-                      icon={faMapMarkerAlt}
-                      className={styles.icon}
-                    />
+                  <Link href="/search/search" className={`${styles.gnbItem} ${styles.link}`}>
+                    <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
                   </Link>
-                  <Link
-                    href="/post"
-                    className={`${styles.gnbItem} ${styles.link}`}
-                  >
+                  <Link href="/post" className={`${styles.gnbItem} ${styles.link}`}>
                     <FontAwesomeIcon icon={faComment} className={styles.icon} />
                   </Link>
                 </nav>
                 <div className={styles.searchBtnBox}>
-                  <Link
-                    href="#"
-                    className={`${styles.btnSearch} ${styles.link}`}
-                  >
-                    <FontAwesomeIcon icon={faSearch} className={styles.icon} />
+                  <Link href="#" className={`${styles.btnSearch} ${styles.link}`}>
+                    <FontAwesomeIcon icon={faSearch} className={styles.icon} onClick={() => changeState()} />
                   </Link>
                 </div>
               </div>
