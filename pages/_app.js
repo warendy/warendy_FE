b2476aa121b4407fa307a13aa6d659feea0e0eb5
Kyungjin Jung <kyoooungjin@gmail.com>
2023-08-12 03:38:41 +0900

    🚑 헤더 푸터 오류 긴급수정

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { RecoilPersist } from "recoil-persist";
+import Header from "../components/header/Header.js";
+import Footer from "../components/footer/Footer.js";
+// import { userTokenState } from "../recoil/atoms"; // Recoil Atom을 임포트합니다.
+import "../styles/reset.css";
+import "../styles/main.css";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <RecoilRoot>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+      {/* <RecoilPersist /> */}
+    </RecoilRoot>
+  );
+};
+
+export default MyApp;
