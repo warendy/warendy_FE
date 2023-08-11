1fd34fa3e9496187efd96169fda9ee16d63d1455
Kyungjin Jung <kyoooungjin@gmail.com>
2023-08-12 00:16:44 +0900

    🎨 코드 구조 개선

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default ({ mode }) => {
-  if (mode === "development") {
-    dotenv.config();
-  }
-};
