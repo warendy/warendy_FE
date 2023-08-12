4beec7cb950f9964280ef11ba6e8b623a251e1ab
soozii <tndwjd963@gmail.com>
2023-08-12 17:42:41 +0900

    :memo: Docs 파일 추가

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = {
-  images: {
-    domains: ["images.vivino.com"],
-  },
+  // next.config.js
+
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://warendy.shop/api/:path*",
+        source: "/api/:path*", // 이 부분에는 프록시하고자 하는 경로를 지정합니다
+        destination: "https://warendy.shop/:path*", // 실제 API 서버의 URL을 여기에 적습니다.
       },
     ];
+  },
+  env: {
+    KAKAO_MAP_KEY: "99d585418706339ae15d964c524e4848",
+    API_KEY: "https://warendy.shop/",
   },
 };
