5ae3d0657a474b94b79fb2ead21ae8a9268755e7
soozii <tndwjd963@gmail.com>
2023-08-16 01:08:10 +0900

    :bug: Fix API 버그 수정

--- pages/search/search.js
+++ pages/search/search.js
@@ -46,12 +46,11 @@ export default function Search() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const wineBarsData = await fetchNearbyWineStores(
-          userLocation.longitude,
-          userLocation.latitude
+        const { data } = await axios.get(
+          `https://warendy.shop/winebars/around?lnt=${userLocation.longitude}&lat=${userLocation.latitude}`
         );
-        console.log(wineBarsData);
-        setFilteredWineBars(wineBarsData);
+        console.log(data);
+        setFilteredWineBars(data);
       } catch (error) {
         console.error("Error fetching nearby wine stores:", error);
       }
