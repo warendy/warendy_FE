0ebbd1f5d1baa35f2d49c47cd23d9ad19f61a0a1
Kyungjin Jung <123256640+Februaar@users.noreply.github.com>
2023-08-16 03:39:05 +0900

    🎨 코드 수정

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,15 +3,13 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="inner">
-        <div className={styles.footerBox}>
-          <div>Copyright © 2023 Warendy Inc. All rights reserved.</div>
-          <div className={styles.policy}>
-            Privacy Policy | Terms of Use | Legal{" "}
-          </div>
-          <div>Republic of Korea</div>
+    <footer className={styles.footer}>
+      <div className={styles.footerBox + " inner "}>
+        <div>Copyright © 2023 Warendy Inc. All rights reserved.</div>
+        <div className={styles.policy}>
+          Privacy Policy | Terms of Use | Legal{" "}
         </div>
+        <div>Republic of Korea</div>
       </div>
     </footer>
   );
