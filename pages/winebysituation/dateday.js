2356cbba314c1e008a89b7742af6dd444c85d380
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-08-16 04:20:34 +0900

    🔧Moved File

--- pages/winebysituation/dateday.js
+++ pages/winebysituation/dateday.js
@@ -1,14 +1,8 @@
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "./winebysituation.module.css";
-import {
-  addWineToFavorite,
-  getWineDetail,
-  getRecommendedWineList,
-} from "@/services/api";
-import { wineListState } from "@/recoil/atoms";
-import { useRecoilValue } from "recoil";
-import axios from "axios";
+import { getRecommendedWineList } from "@/services/api";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function WineBySituation() {
   const [wines, setWines] = useState([]);
@@ -17,9 +11,9 @@ export default function WineBySituation() {
     const fetchWines = async () => {
       try {
         const recommendedWines = await getRecommendedWineList();
-        const filteredWines = recommendedWines.filter(
-          (wine) => wine.body >= 2 && wine.tannin >= 2
-        );
+        console.log("Recommended Wines:", recommendedWines);
+        const filteredWines = recommendedWines.filter((wine) => wine.tannin <= 2 && wine.alcohol >= 13);
+        console.log(filteredWines);
         setWines(filteredWines);
       } catch (error) {
         console.error("와인 정보를 가져오는 데 실패했습니다:", error);
@@ -37,12 +31,12 @@ export default function WineBySituation() {
       </div>
       <div className={styles.wineExplainContainer}>
         <div className={styles.wineExplainContents}>
-          <h2 className="title">데이트하는 날</h2>
+          <h2 className="title">데이트 하는 날</h2>
           <h1 className={styles.wineName}>까베르네 쇼비뇽</h1>
           <h3 className={styles.wineSub}>
-            데이트 식사에서 스테이크나 파스타를 드신다면
+            레스토랑 같은 분위기에서
             <br />
-            풍부한 탄닌의 와인으로 균형을 이뤄보세요.
+            어울릴 만한 와인을 추천해드릴게요.
           </h3>
         </div>
       </div>
@@ -50,12 +44,10 @@ export default function WineBySituation() {
         <ul className={styles.lovedWineList}>
           {wines.map((wine) => (
             <li key={wine.id} className={styles.lovedWineItem}>
-              <Image
-                src={wine.picture}
-                alt={`Wine ${wine.id}`}
-                width={100}
-                height={250}
-              />
+              <Link href={`/detail/${wine.id}`}>
+                {" "}
+                <Image src={wine.picture} alt={`Wine ${wine.id}`} width={100} height={250} />
+              </Link>
             </li>
           ))}
         </ul>
