f94b2fd507b895aeb2272b19f1012d316f820438
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-08-16 04:23:05 +0900

    🔧Moved file

--- pages/winebysituation/drinkingday.js
+++ pages/winebysituation/drinkingday.js
@@ -1,14 +1,8 @@
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "./winebysituation.module.css";
-import {
-  addWineToFavorite,
-  getWineDetail,
-  getRecommendedWineList,
-} from "@/services/api";
-import { wineListState } from "@/recoil/atoms";
-import { useRecoilValue } from "recoil";
-import axios from "axios";
+import { getRecommendedWineList } from "@/services/api";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function WineBySituation() {
   const [wines, setWines] = useState([]);
@@ -17,9 +11,7 @@ export default function WineBySituation() {
     const fetchWines = async () => {
       try {
         const recommendedWines = await getRecommendedWineList();
-        const filteredWines = recommendedWines.filter(
-          (wine) => wine.alcohol >= 14
-        );
+        const filteredWines = recommendedWines.filter((wine) => wine.alcohol >= 14);
         setWines(filteredWines);
       } catch (error) {
         console.error("와인 정보를 가져오는 데 실패했습니다:", error);
@@ -40,8 +32,9 @@ export default function WineBySituation() {
           <h2 className="title">취하고 싶은 날</h2>
           <h1 className={styles.wineName}>까베르네 쇼비뇽</h1>
           <h3 className={styles.wineSub}>
-            한 모금 마실 때마다 시간이 멈춘 듯<br />
-            풍미가 남아 있는 동안 혀의 끝에서 탱고가 펼쳐집니다.
+            오늘 잊고 싶은 기억이 있다면
+            <br />
+            높은 알코올로 한번쯤 지워보는 것도 좋아요.
           </h3>
         </div>
       </div>
@@ -49,12 +42,10 @@ export default function WineBySituation() {
         <ul className={styles.lovedWineList}>
           {wines.map((wine) => (
             <li key={wine.id} className={styles.lovedWineItem}>
-              <Image
-                src={wine.picture}
-                alt={`Wine ${wine.id}`}
-                width={100}
-                height={250}
-              />
+              <Link href={`/detail/${wine.id}`}>
+                {" "}
+                <Image src={wine.picture} alt={`Wine ${wine.id}`} width={100} height={250} />
+              </Link>
             </li>
           ))}
         </ul>
