bb7cb6646bf57f69bdb02259a74ce20f31e7ecbe
Kyungjin Jung <123256640+Februaar@users.noreply.github.com>
2023-08-16 01:36:00 +0900

    🎨 코드 수정

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import Main from "./main/main";
-import { worker } from "../mocks/handlers";
-
-if (worker) {
-  worker.start();
-}
 
 export default function Index() {
   return (
