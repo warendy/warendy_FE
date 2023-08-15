2474db7d569284e451f701fdac70bc9d660671ad
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-08-16 04:04:47 +0900

    🔧Moved file

--- pages/main/loved-wine.js
+++ pages/main/loved-wine.js
@@ -3,25 +3,17 @@ import styles from "./loved-wine.module.css";
 import Link from "next/link";
 
 export default function LovedWine({ List }) {
-  console.log(List);
-
   return (
     <div className={styles.lovedWineContainer}>
       <div className={styles.lovedWines}>
-        <h3 className="title">많은 사랑을 받고 있는 와인</h3>
+        <h3 className="title">많은 사랑을 받고 있어요!</h3>
         <ul className={styles.lovedWineList}>
           {List &&
-            List.map((el, idx) => (
+            List.slice(0, 10).map((el, idx) => (
               <li key={idx} className={styles.lovedWineItem}>
-                <Link href={`/detail/${el.id}`}>
-                  {" "}
+                <Link href={`/detail/${el.id}`} legacyBehavior>
                   <a>
-                    <Image
-                      src={el.picture}
-                      alt={el.wineName}
-                      width={100}
-                      height={230}
-                    />
+                    <Image src={el.picture} alt={el.wineName} width={100} height={230} />
                     <p>{el.wineName}</p>
                   </a>
                 </Link>
