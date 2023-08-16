e98933d23be08eb72d5f88fdb4b87df88273f10b
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-08-16 15:56:34 +0900

    🔧Moved file

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,19 +2,28 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useRecoilState } from "recoil";
-import { userTokenState } from "../../recoil/atoms";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { userTokenState } from "@/recoil/atoms";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faComment } from "@fortawesome/free-regular-svg-icons";
-import styles from "./Header.module.css";
+import { faMapMarkerAlt, faSearch, faComment } from "@fortawesome/free-solid-svg-icons";
+import styles from "./header.module.css";
 import { LogoutModal } from "../Modal";
+import { searchBarState, slideState } from "@/recoil/searchbar";
 
 const Header = () => {
   const [userToken, setUserToken] = useRecoilState(userTokenState);
   const [mounted, setMounted] = useState(false);
+
   const [showLogoutModal, setShowLogoutModal] = useState(false);
   const router = useRouter();
+  const setSearchBarState = useSetRecoilState(searchBarState);
+  const setAnimationClass = useSetRecoilState(slideState);
+
+  const changeState = (e) => {
+    e.preventDefault();
+    setSearchBarState(true);
+    setAnimationClass("slidein");
+  };
 
   const handleLogout = () => {
     sessionStorage.removeItem("userTokenState");
@@ -42,7 +51,7 @@ const Header = () => {
                 마이페이지
               </Link>
               {userToken ? (
-                <button onClick={handleLogout} className="resetBtn">
+                <button onClick={handleLogout} className={styles.link + " resetBtn "}>
                   로그아웃
                 </button>
               ) : (
@@ -55,45 +64,21 @@ const Header = () => {
               <div className={styles.mainInner}>
                 <h1 className={styles.mainLogo}>
                   <Link href="/" className={styles.logo}>
-                    <Image
-                      src="/images/logo.svg"
-                      alt="Logo"
-                      className={styles.img}
-                      width={100}
-                      height={70}
-                    />
+                    <Image src="/images/logo.svg" alt="Logo" className={styles.img} width={100} height={70} />
                   </Link>
                 </h1>
                 <div className={styles.gnbArea}>
                   <nav className={styles.gnb}>
-                    <Link
-                      href="/search/search"
-                      className={`${styles.gnbItem} ${styles.link}`}
-                    >
-                      <FontAwesomeIcon
-                        icon={faMapMarkerAlt}
-                        className={styles.icon}
-                      />
+                    <Link href="/search/search" className={`${styles.gnbItem} ${styles.link}`}>
+                      <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
                     </Link>
-                    <Link
-                      href="/post"
-                      className={`${styles.gnbItem} ${styles.link}`}
-                    >
-                      <FontAwesomeIcon
-                        icon={faComment}
-                        className={styles.icon}
-                      />
+                    <Link href="/post" className={`${styles.gnbItem} ${styles.link}`}>
+                      <FontAwesomeIcon icon={faComment} className={styles.icon} />
                     </Link>
                   </nav>
                   <div className={styles.searchBtnBox}>
-                    <Link
-                      href="#"
-                      className={`${styles.btnSearch} ${styles.link}`}
-                    >
-                      <FontAwesomeIcon
-                        icon={faSearch}
-                        className={styles.icon}
-                      />
+                    <Link href="#" className={`${styles.btnSearch} ${styles.link}`} onClick={changeState}>
+                      <FontAwesomeIcon icon={faSearch} className={styles.icon} />
                     </Link>
                   </div>
                 </div>
