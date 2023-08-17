f06f4c547cc8740f50f5947462074bf427fdb49f
Kyungjin Jung <kyoooungjin@gmail.com>
2023-08-17 17:17:33 +0900

    ✨ 무한스크롤 기능 다시 추가

--- pages/review-page/index.js
+++ pages/review-page/index.js
@@ -9,14 +9,12 @@ import styles from "./index.module.css";
 
 const ReviewPage = () => {
   const [wineReviews, setWineReviews] = useState([]);
-  console.log(wineReviews);
   const [filteredReviews, setFilteredReviews] = useState([]);
   const [filterOptions, setFilterOptions] = useState({
     wineType: "",
     winePairing: "",
     wineRating: "",
   });
-
   const token = useRecoilValue(userTokenState);
 
   useEffect(() => {
@@ -53,7 +51,7 @@ const ReviewPage = () => {
       return wineTypeCondition && winePairingCondition && wineRatingCondition;
     });
 
-    setFilteredReviews(filtered);
+    setFilteredReviews(filtered.slice(0, 3));
   };
 
   useEffect(() => {
@@ -61,8 +59,6 @@ const ReviewPage = () => {
   }, [filterOptions, wineReviews]);
 
   const handleSaveReview = async (reviewId, updatedContent) => {
-    console.log(reviewId);
-    console.log(updatedContent);
     if (reviewId !== undefined) {
       try {
         await updateMyReview(reviewId, updatedContent, updatedRating, token);
@@ -90,6 +86,31 @@ const ReviewPage = () => {
       console.error("Error deleting review:", error);
     }
   };
+
+  const loadMoreReviews = () => {
+    const startIndex = filteredReviews.length;
+    const endIndex = startIndex + 3;
+    setFilteredReviews((prevFiltered) => [
+      ...prevFiltered,
+      ...wineReviews.slice(startIndex, endIndex),
+    ]);
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 100
+    ) {
+      loadMoreReviews();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <Layout>
